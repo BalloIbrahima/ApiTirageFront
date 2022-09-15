ce6996a35883ebaf400e0377dd684c455735ea1e
mttraore <marytra22@gmail.com>
2022-09-15 19:36:01 +0000

    mes modif 4

--- src/app/import-tire/import-tire.component.ts
+++ src/app/import-tire/import-tire.component.ts
@@ -27,7 +27,6 @@ export class ImportTireComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    
 
     newTirage(f:NgForm){
       this.tirageService.create(this.libelle,this.nbrPostulantNew,this.fichier).subscribe(data=>{
